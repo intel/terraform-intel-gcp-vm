5b217aed10621f1b6c0384cb0a52a06131d3fc9b
JoshHilliker <josh.hilliker@intel.com>
2023-01-26 10:42:38 -0800

    preparing for new example

---
+++ examples/gcp-windows-vm-non-default-network/variables.tf
@@ -0,0 +1,15 @@
+########################
+####     Intel      ####
+########################
+variable "project" {
+  description = "Project id in GCP"
+  type        = string
+  value       = "intel-csa-resource-GCP"
+}
+########################
+####    Required    ####
+########################
+
+########################
+####     Other      ####
+########################
