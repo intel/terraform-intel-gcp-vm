0ef6cba849ce8d34d537fd1395a34b1cb532e3ef
JoshHilliker <josh.hilliker@intel.com>
2023-01-30 11:02:46 -0800

    quick changes for testing.

--- examples/gcp-linux-vm-create-network/variables.tf
+++ examples/gcp-linux-vm-create-network/variables.tf
@@ -1,11 +1,7 @@
 ########################
 ####     Intel      ####
 ########################
-variable "project" {
-  description = "Project id in GCP"
-  type        = string
-  value       = "intel-csa-resource-GCP"
-}
+
 ########################
 ####    Required    ####
 ########################
