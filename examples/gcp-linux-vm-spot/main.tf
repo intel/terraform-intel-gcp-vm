608ff329ddcc331b5783a744193dfcc643685071
kalenarndt <kalenarndt@gmail.com>
2023-02-01 13:59:42 -0700

    pre-call

--- examples/gcp-linux-vm-spot/main.tf
+++ examples/gcp-linux-vm-spot/main.tf
@@ -2,4 +2,7 @@ module "spot-vm" {
   source = "../../"
   boot_image_family = "ubuntu-2004-lts"
   name = "awesome-vm"
+  network = "default"
+  # nic_type = "GVNIC"
+
 }
--- examples/gcp-linux-vm-spot/providers.tf
+++ examples/gcp-linux-vm-spot/providers.tf
@@ -1,5 +1,3 @@
 provider "google" {
-  # Configuration options
-  project = "intel-csa-resource-gcp"
   zone = "us-central1-a"
 }
