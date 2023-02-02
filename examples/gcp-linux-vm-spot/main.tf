ccb06c0fc3dd5843966af715dbac27d551badab4
kalenarndt <kalenarndt@gmail.com>
2023-02-02 11:13:06 -0700

    post meeting

--- examples/gcp-linux-vm-spot/main.tf
+++ examples/gcp-linux-vm-spot/main.tf
@@ -1,6 +1,6 @@
 module "spot-vm" {
   source              = "../../"
-  project             = var.project
+  # project             = var.project
   boot_image_family   = "ubuntu-2004-lts"
   name                = "this-is-a-spot-vm"
   network             = "default"
