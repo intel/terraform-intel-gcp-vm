763c535d10ba2dea51c8b379c112b8e629b319c9
Rajiv Mandal <rajiv.mandal@intel.com>
2023-02-03 11:49:20 -0800

    PR addresses fix for default value of termination action and fix external public ip issue (#3)
    * fixed default value of termination action and fix external public ip issue
    
    * terraform-docs: automated action
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- examples/gcp-linux-vm/main.tf
+++ examples/gcp-linux-vm/main.tf
@@ -5,4 +5,9 @@ module "linux-vm" {
   name                = "this-is-a-linux-vm"
   network             = "default"
   on_host_maintenance = "TERMINATE"
+  access_config = [{
+    nat_ip                 = var.nat_ip
+    public_ptr_domain_name = var.public_ptr_domain_name
+    network_tier           = var.network_tier
+  }, ]
 }
