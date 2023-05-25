4de5b934161dbb581aceb24d18ac187289abd91e
Alexander Goldberg <alexander.goldberg@intel.com>
2023-05-25 09:04:08 -0700

    change debian 10 to ubuntu 22.04

--- examples/gcp-linux-vm/main.tf
+++ examples/gcp-linux-vm/main.tf
@@ -1,10 +1,10 @@
 # You will need to provide value of the variable project, which is your GCP project id when you do terraform apply
 
 module "linux_vm" {
-  source              = "intel/gcp-vm/intel"
+  source              = "../../"
   project             = var.project
-  boot_image_family   = "ubuntu-2004-lts"
-  name                = "this-is-a-Saphire-Rapids-vm-Booyahv4"
+  boot_image_family   = "ubuntu-2204-lts"
+  name                = "saph-rapids-vm"
   access_config = [{
     nat_ip                 = null
     public_ptr_domain_name = null
