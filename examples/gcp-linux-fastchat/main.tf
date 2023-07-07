2a587b5608d283e4f1da516ad78d6ce49184800a
Alex Goldberg <alexander.goldberg@intel.com>
2023-07-07 19:35:29 -0400

    add firwall rules

--- examples/gcp-linux-fastchat/main.tf
+++ examples/gcp-linux-fastchat/main.tf
@@ -8,6 +8,18 @@ data "template_file" "user_data" {
   template = file("./cloud_init.yml")
 }
 
+resource "google_compute_firewall" "allow_http" {
+  name    = "allow-http-rule"
+  network = "default"
+  allow {
+    ports    = ["3000", "3001", "3002"]
+    protocol = "tcp"
+  }
+  target_tags = ["allow-http"]
+  priority    = 1000
+
+}
+
 module "linux_vm" {
   source              = "../../"
   project             = var.project
@@ -16,6 +28,7 @@ module "linux_vm" {
   name                = "intel-fastchat-test"
   zone                = "us-central1-a"
   machine_type        = "c3-highcpu-44"
+  tags                = ["allow-http"]
   user_data    = data.template_file.user_data.rendered
   access_config = [{
     nat_ip                 = null
