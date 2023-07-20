37e767abbe5efa883383a7c09bba1a070da9303a
Alex Goldberg <alexander.goldberg@intel.com>
2023-07-20 15:12:43 -0700

    changed structure of ansible clone

--- examples/gcp-linux-fastchat/main.tf
+++ examples/gcp-linux-fastchat/main.tf
@@ -19,7 +19,7 @@ module "linux_vm" {
   boot_image_family   = "ubuntu-2204-lts"
   name                = "intel-fastchat-test"
   zone                = "us-central1-a"
-  machine_type        = "c3-highcpu-88"
+  machine_type        = "c3-highmem-8"
   network_ip          = "10.128.15.213"      
   tags                = ["fschat"]
   user_data    = data.template_file.user_data.rendered
@@ -37,7 +37,7 @@ module "linux_vm_n2" {
   boot_image_family   = "ubuntu-2204-lts"
   name                = "intel-fastchat-older-node"
   zone                = "us-central1-a"
-  machine_type        = "n2-highcpu-96"
+  machine_type        = "n2-highmem-8"
   network_ip          = "10.128.15.214"
   tags                = ["fschat"]
   user_data    = data.template_file.user_data_worker.rendered
---
+++ examples/gcp-linux-with-aikit/main.tf
@@ -0,0 +1,24 @@
+variable "project" {
+  type        = string
+  description = "project_id"
+  default = "intel-csa-resource-gcp"
+}
+
+data "template_file" "user_data" {
+  template = file("./cloud_init.yml")
+}
+
+module "linux_vm" {
+  source              = "../../"
+  project             = var.project
+  boot_image_project  = "ubuntu-os-cloud"
+  boot_image_family   = "ubuntu-2204-lts"
+  name                = "intel-terra-test"
+  zone                = "us-central1-a"
+  user_data    = data.template_file.user_data.rendered
+  access_config = [{
+    nat_ip                 = null
+    public_ptr_domain_name = null
+    network_tier           = "PREMIUM"
+  }, ]
+}
