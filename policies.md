7bbbda9c0b04144940391efbc69c8de1d5ea9383
Kinder Wischmeier <kinder.wischmeier@intel.com>
2023-08-08 17:11:17 -0400

    Update VM SKUs
    * added new c3 vms to policies
    
    * added gp with local ssd to policies and variables
    
    * Update policies.md
    
    * changed machine type default in variables
    
    * fixed typo in policies and variables
    
    * terraform-docs: automated action
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- policies.md
+++ policies.md
@@ -21,10 +21,14 @@ Parameter: machine_type
 Allowed Types
 
 - **General Purpose:**  N2-Standard:  n2-standard-2, n2-standard-4, n2-standard-8, n2-standard-16, n2-standard-32, n2-standard-48, n2-standard-64, n2-standard-80, n2-standard-96, n2-standard-128
-- **General Purpose:** N2-HighMemory: n2-highmem-2, n2-highmem-4, n2-highmem-8, n2-highmem-16, n2-highmem-32, n2-highmem-48, n2-highmem-64, n2-highmem-80, n2-highmem-96, n2-highmem-128
+- **General Purpose:** N2-HighMem: n2-highmem-2, n2-highmem-4, n2-highmem-8, n2-highmem-16, n2-highmem-32, n2-highmem-48, n2-highmem-64, n2-highmem-80, n2-highmem-96, n2-highmem-128
 - **General Purpose:** N2-HighCPU: n2-highcpu-2, n2-highcpu-4, n2-highcpu-8, n2-highcpu-16, n2-highcpu-32, n2-highcpu-48, n2-highcpu-64, n2-highcpu-80, n2-highcpu-96
+- **General Purpose:** C3-Standard:
+c3-standard-4, c3-standard-8, c3-standard-22, c3-standard-44, c3-standard-88, c3-standard-176
+- **General Purpose with Local SSD:** C3-Standard-LSSD: c3-standard-4-lssd, c3-standard-8-lssd, c3-standard-22-lssd, c3-standard-44-lssd, c3-standard-88-lssd, c3-standard-176-lssd
 - **Compute Optimized:** C3-HighCPU: c3-highcpu-4, c3-highcpu-8, c3-highcpu-22, c3-highcpu-44, c3-highcpu-88, c3-highcpu-176
-- **Memory Optimized:** m3-ultramem-32, m3-ultramem-64, m3-ultramem-128, m3-megamem-64, m3-megamem-128
+- **Memory Optimized:** c3-HighMem: c3-highmem-4, c3-highmem-8, c3-highmem-22, c3-highmem-44, c3-highmem-88, c3-highmem-176
+- **Memory Optimized:** m3-UltraMem: m3-ultramem-32, m3-ultramem-64, m3-ultramem-128, m3-megamem-64, m3-megamem-128
 
 ## Policy 2
 
--- variables.tf
+++ variables.tf
@@ -3,11 +3,14 @@
 ########################
 # See policies.md, Intel recommends the 3rd Generation Intel® Xeon® Platinum (Ice Lake) based instances. It is the latest Intel CPU generation globally available
 # at the time of creation of this repo
-# General Purpose N2-Standard:  n2-standard-2, n2-standard-4, n2-standard-8, n2-standard-16, n2-standard-32, n2-standard-48, n2-standard-64, n2-standard-80, n2-standard-96, n2-standard-128
-# General Purpose N2-HighMemory: n2-highmem-2, n2-highmem-4, n2-highmem-8, n2-highmem-16, n2-highmem-32, n2-highmem-48, n2-highmem-64, n2-highmem-80, n2-highmem-96, n2-highmem-128
-# General Purpose N2-HighCPU: n2-highcpu-2, n2-highcpu-4, n2-highcpu-8, n2-highcpu-16, n2-highcpu-32, n2-highcpu-48, n2-highcpu-64, n2-highcpu-80, n2-highcpu-96
-# Compute Optimized C3-HighCPU: c3-highcpu-4, c3-highcpu-8, c3-highcpu-22, c3-highcpu-44, c3-highcpu-88, c3-highcpu-176, C2-standard: c2-standard-4, c2-standard-8, c2-standard-16, c2-standard-30, c2-standard-60
-# Memory Optimized m3-ultramem-32, m3-ultramem-64, m3-ultramem-128, m3-megamem-64, m3-megamem-128
+#General Purpose: N2-Standard:  n2-standard-2, n2-standard-4, n2-standard-8, n2-standard-16, n2-standard-32, n2-standard-48, n2-standard-64, n2-standard-80, n2-standard-96, n2-standard-128
+#General Purpose: N2-HighMem: n2-highmem-2, n2-highmem-4, n2-highmem-8, n2-highmem-16, n2-highmem-32, n2-highmem-48, n2-highmem-64, n2-highmem-80, n2-highmem-96, n2-highmem-128
+#General Purpose: N2-HighCPU: n2-highcpu-2, n2-highcpu-4, n2-highcpu-8, n2-highcpu-16, n2-highcpu-32, n2-highcpu-48, n2-highcpu-64, n2-highcpu-80, n2-highcpu-96
+#General Purpose: C3-Standard:c3-standard-4, c3-standard-8, c3-standard-22, c3-standard-44, c3-standard-88, c3-standard-176
+#General Purpose with Local SSD: C3-Standard-LSSD: c3-standard-4-lssd, c3-standard-8-lssd, c3-standard-22-lssd, c3-standard-44-lssd, c3-standard-88-lssd, c3-standard-176-lssd
+#Compute Optimized: C3-HighCPU: c3-highcpu-4, c3-highcpu-8, c3-highcpu-22, c3-highcpu-44, c3-highcpu-88, c3-highcpu-176
+#Memory Optimized: c3-HighMem: c3-highmem-4, c3-highmem-8, c3-highmem-22, c3-highmem-44, c3-highmem-88, c3-highmem-176
+#Memory Optimized: m3-UltraMem, m3-ultramem-32, m3-ultramem-64, m3-ultramem-128, m3-megamem-64, m3-megamem-128
 
 #"minCpuPlatform": Intel Cascade Lake, Intel Ice Lake, Intel Sapphire Rapids
 # as of May 23, 2023 C3 is available in us-east1-b, us-east1-c, us-east1-d, us-east4-c, us-east4-b, us-east4-a, us-central1-c, us-central1-a, us-central1-b, europe-west4-a, europe-west4-b, europe-west4-c, europe-west1-b, europe-west1-d, europe-west1-c, asia-southeast1-b, asia-southeast1-a, asia-southeast1-c
@@ -16,7 +19,7 @@
 variable "machine_type" {
   type        = string
   description = "The machine type to create"
-  default     = "c3-highcpu-4"
+  default     = "c3-standard-4"
 }
 
 ########################
