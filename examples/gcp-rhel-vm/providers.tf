e50028d1f03e9a8292a1f60e0cdfaaf263623fe3
Kinder Wischmeier <kinder.wischmeier@intel.com>
2023-08-30 16:07:16 -0400

    fixed provider details

--- examples/gcp-rhel-vm/providers.tf
+++ examples/gcp-rhel-vm/providers.tf
@@ -1,6 +1,3 @@
 provider "google" {
-  zone        = "us-east1-b"
-  region      = "us-east1"
-  project     = "intel-csa-resource-gcp"
-  credentials = file("C:/Users/kwischme/Downloads/intel-csa-resource-gcp-4f110abc317a.json")
+  zone = "us-central1-a"
 }
