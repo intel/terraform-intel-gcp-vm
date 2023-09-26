069af62c1cfc7193d8e4e94009b60f41c2ebd0a5
Lucas Melo <lucas.melo@intel.com>
2023-09-26 09:09:05 -0500

    Simplify README.md and main.tf (#30)
    * Simplify README.md and main.tf
    
    * Edit README.md

--- examples/gcp-linux-fastchat-simple/main.tf
+++ examples/gcp-linux-fastchat-simple/main.tf
@@ -10,9 +10,9 @@ module "linux_vm" {
   project             = var.project
   boot_image_project  = "ubuntu-os-cloud"
   boot_image_family   = "ubuntu-2204-lts"
-  name                = "intel-fastchat-test"
+  name                = "intel-fastchat"
   zone                = "us-central1-a"
-  machine_type        = "c3-highmem-22"  
+  machine_type        = "c3-standard-44"  
   tags                = ["fschat"]
   user_data           = templatefile("./cloud_init.yml", {})
   access_config = [{
@@ -22,18 +22,16 @@ module "linux_vm" {
   }, ]
 }
 
-# FastChat Gradio Webserver can be accessed by the Gradio Proxy. See https://xxxxxxx.gradio.live URL that is generated during the gradio run
-# Otherwise uncomment this to allow access to the VM Public IP on Port 7860, http://<VM_PLUBLIC_IP>:7860
-# resource "google_compute_firewall" "rules" {
-#   project     =  var.project
-#   name        = "fastchat-firewall"
-#   network     = "default"
-#   description = "Allows access to FastChat Webserver."
+resource "google_compute_firewall" "rules" {
+  project     =  var.project
+  name        = "fastchat-firewall"
+  network     = "default"
+  description = "Allows access to FastChat Webserver."
 
-#   allow {
-#     protocol  = "tcp"
-#     ports     = ["7860"]
-#   }
-#   source_ranges = [ "0.0.0.0/0" ]
-#   target_tags = ["fschat"]
-# }
+  allow {
+    protocol  = "tcp"
+    ports     = ["7860"]
+  }
+  source_ranges = [ "0.0.0.0/0" ]
+  target_tags = ["fschat"]
+}
