41269f9470ecb34e9e84e6df0af6d92edcb7bd07
mneru002 <mihika.nerurkar@intel.com>
2025-08-18 22:37:50 -0700

    Updated vm-spot

--- examples/gcp-linux-vm-spot/providers.tf
+++ examples/gcp-linux-vm-spot/providers.tf
@@ -1,10 +1,3 @@
-# provider "google" {
-#   zone = "us-central1-a"
-# }
-
 provider "google" {
-  credentials = file("/home/mihikanerurkar/code/intel-csa-resource-gcp.json")
-  project     = "intel-csa-resource-gcp"
-  region      = "us-central1"
-  zone        = "us-central1-b"
+  zone = "us-central1-a"
 }
